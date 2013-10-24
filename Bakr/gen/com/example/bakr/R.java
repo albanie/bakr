70ee1e69d021bada0d60f70b9ad414e965aa84b4
Sam <satiricallaughter@gmail.com>
2013-10-24 16:56:24 +0100

    Added more recipe shizzle

--- Bakr/gen/com/example/bakr/R.java
+++ Bakr/gen/com/example/bakr/R.java
@@ -10,6 +10,10 @@ package com.example.bakr;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int background1=0x7f060000;
+        public static final int background2=0x7f060001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -41,30 +45,47 @@ public final class R {
         public static final int title=0x7f020011;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
-        public static final int imageView1=0x7f080001;
-        public static final int imageView2=0x7f080000;
-        public static final int imageView3=0x7f080002;
-        public static final int imageView4=0x7f080003;
-        public static final int imageView5=0x7f080004;
-        public static final int imageView6=0x7f080005;
-        public static final int imageView7=0x7f080006;
+        public static final int action_settings=0x7f090010;
+        public static final int buttonfive=0x7f09000f;
+        public static final int buttonfour=0x7f09000e;
+        public static final int buttonone=0x7f09000b;
+        public static final int buttonthree=0x7f09000d;
+        public static final int buttontwo=0x7f09000c;
+        public static final int chronometer1=0x7f09000a;
+        public static final int content=0x7f090008;
+        public static final int handle=0x7f090009;
+        public static final int imageView1=0x7f090001;
+        public static final int imageView2=0x7f090000;
+        public static final int imageView3=0x7f090002;
+        public static final int imageView4=0x7f090003;
+        public static final int imageView5=0x7f090004;
+        public static final int imageView6=0x7f090005;
+        public static final int imageView7=0x7f090006;
+        public static final int slidingDrawer1=0x7f090007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int recipe_five=0x7f030001;
-        public static final int recipe_four=0x7f030002;
-        public static final int recipe_one=0x7f030003;
-        public static final int recipe_three=0x7f030004;
-        public static final int recipe_two=0x7f030005;
+        public static final int activity_menu=0x7f030001;
+        public static final int recipe_five=0x7f030002;
+        public static final int recipe_four=0x7f030003;
+        public static final int recipe_one=0x7f030004;
+        public static final int recipe_three=0x7f030005;
+        public static final int recipe_two=0x7f030006;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
+        public static final int menu=0x7f080001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int more_handle=0x7f050004;
+        public static final int showalert=0x7f050009;
+        public static final int showblack=0x7f050008;
+        public static final int showimage1=0x7f050005;
+        public static final int showimage2=0x7f050006;
+        public static final int showwhite=0x7f050007;
         public static final int title_activity_menu=0x7f050003;
     }
     public static final class style {
@@ -89,10 +110,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
